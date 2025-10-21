08e10b221c292194ff2985839417c9f4c7b02ee3
mariobot <mariobot113@hotmail.com>
2025-10-21 06:39:35 -0500

    Add queries to get information from the user.

---
+++ Hyip-Payments.Query/UserQuery/GetUserByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using Hyip_Payments.Context;
+using Hyip_Payments.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hyip_Payments.Query.UserQuery
+{
+    // MediatR query to get a user by ID
+    public class GetUserByIdQuery : IRequest<UserModel?>
+    {
+        public int UserId { get; }
+        public GetUserByIdQuery(int userId) => UserId = userId;
+    }
+
+    // Handler
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserModel?>
+    {
+        private readonly PaymentsDbContext _context;
+
+        public GetUserByIdQueryHandler(PaymentsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserModel?> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Id == request.UserId, cancellationToken);
+        }
+    }
+}
---
+++ Hyip-Payments.Query/UserQuery/GetUserListQuery.cs
@@ -0,0 +1,30 @@
+﻿using Hyip_Payments.Context;
+using Hyip_Payments.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hyip_Payments.Query.UserQuery
+{
+    // MediatR query to get all users
+    public class GetUserListQuery : IRequest<List<UserModel>>
+    {
+    }
+
+    // Handler
+    public class GetUserListQueryHandler : IRequestHandler<GetUserListQuery, List<UserModel>>
+    {
+        private readonly PaymentsDbContext _context;
+
+        public GetUserListQueryHandler(PaymentsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserModel>> Handle(GetUserListQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}

55f261bca8b7684c88b3e0e913fb742d081e68c7
mariobot <mariobot113@hotmail.com>
2025-10-21 06:18:22 -0500

    Add the queries GetInvoiceItem

---
+++ Hyip-Payments.Query/InvoiceItemQuery/GetInvoiceItemByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using Hyip_Payments.Context;
+using Hyip_Payments.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hyip_Payments.Query.InvoiceItemQuery
+{
+    // MediatR query to get an invoice item by ID
+    public class GetInvoiceItemByIdQuery : IRequest<InvoiceItemModel?>
+    {
+        public int InvoiceItemId { get; }
+        public GetInvoiceItemByIdQuery(int invoiceItemId) => InvoiceItemId = invoiceItemId;
+    }
+
+    // Handler
+    public class GetInvoiceItemByIdQueryHandler : IRequestHandler<GetInvoiceItemByIdQuery, InvoiceItemModel?>
+    {
+        private readonly PaymentsDbContext _context;
+
+        public GetInvoiceItemByIdQueryHandler(PaymentsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<InvoiceItemModel?> Handle(GetInvoiceItemByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.InvoiceItems
+                .AsNoTracking()
+                .FirstOrDefaultAsync(i => i.Id == request.InvoiceItemId, cancellationToken);
+        }
+    }
+}
---
+++ Hyip-Payments.Query/InvoiceItemQuery/GetInvoiceItemListQuery.cs
@@ -0,0 +1,30 @@
+﻿using Hyip_Payments.Context;
+using Hyip_Payments.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hyip_Payments.Query.InvoiceItemQuery
+{
+    // MediatR query to get all invoice items
+    public class GetInvoiceItemListQuery : IRequest<List<InvoiceItemModel>>
+    {
+    }
+
+    // Handler
+    public class GetInvoiceItemListQueryHandler : IRequestHandler<GetInvoiceItemListQuery, List<InvoiceItemModel>>
+    {
+        private readonly PaymentsDbContext _context;
+
+        public GetInvoiceItemListQueryHandler(PaymentsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<InvoiceItemModel>> Handle(GetInvoiceItemListQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.InvoiceItems
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}

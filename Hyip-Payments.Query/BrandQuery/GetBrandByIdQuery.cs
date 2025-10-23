9fc6afb3b0b8217c57ccdedc30f41df92def8b10
mariobot <mariobot113@hotmail.com>
2025-10-23 00:23:27 -0500

    Brand queries.

---
+++ Hyip-Payments.Query/BrandQuery/GetBrandByIdQuery.cs
@@ -0,0 +1,31 @@
+﻿using Hyip_Payments.Context;
+using Hyip_Payments.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hyip_Payments.Query.BrandQuery
+{
+    // MediatR query to get a brand by Id
+    public class GetBrandByIdQuery : IRequest<BrandModel?>
+    {
+        public int Id { get; }
+        public GetBrandByIdQuery(int id) => Id = id;
+    }
+
+    public class GetBrandByIdQueryHandler : IRequestHandler<GetBrandByIdQuery, BrandModel?>
+    {
+        private readonly PaymentsDbContext _dbContext;
+
+        public GetBrandByIdQueryHandler(PaymentsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<BrandModel?> Handle(GetBrandByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Brands
+                .AsNoTracking()
+                .FirstOrDefaultAsync(b => b.Id == request.Id, cancellationToken);
+        }
+    }
+}
---
+++ Hyip-Payments.Query/BrandQuery/GetBrandListQuery.cs
@@ -0,0 +1,29 @@
+﻿using Hyip_Payments.Context;
+using Hyip_Payments.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hyip_Payments.Query.BrandQuery
+{
+    // MediatR query to get all brands
+    public class GetBrandListQuery : IRequest<List<BrandModel>>
+    {
+    }
+
+    public class GetBrandListQueryHandler : IRequestHandler<GetBrandListQuery, List<BrandModel>>
+    {
+        private readonly PaymentsDbContext _dbContext;
+
+        public GetBrandListQueryHandler(PaymentsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<BrandModel>> Handle(GetBrandListQuery request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Brands
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+    }
+}

aff76cefef93eea7d170ef0900d59af516058bd0
mariobot <mariobot113@hotmail.com>
2025-11-06 05:36:44 -0500

    Add query

---
+++ Hyip-Payments.Query/WalletQuery/GetWalletQuery.cs
@@ -0,0 +1,38 @@
+﻿using Hyip_Payments.Models;
+using Hyip_Payments.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hyip_Payments.Query.WalletQuery
+{
+    // Query
+    public class GetWalletQuery : IRequest<WalletModel?>
+    {
+        public int WalletId { get; set; }
+
+        public GetWalletQuery(int walletId)
+        {
+            WalletId = walletId;
+        }
+    }
+
+    // Handler
+    public class GetWalletQueryHandler : IRequestHandler<GetWalletQuery, WalletModel?>
+    {
+        private readonly PaymentsDbContext _context;
+
+        public GetWalletQueryHandler(PaymentsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<WalletModel?> Handle(GetWalletQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Wallets
+                .AsNoTracking()
+                .FirstOrDefaultAsync(w => w.Id == request.WalletId, cancellationToken);
+        }
+    }
+}

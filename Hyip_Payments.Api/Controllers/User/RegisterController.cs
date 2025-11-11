3ef2f7b21730ed894245f7ca19e31ca92866cd24
mariobot <mariobot113@hotmail.com>
2025-11-10 21:26:58 -0500

    Add register controller

---
+++ Hyip_Payments.Api/Controllers/User/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using Hyip_Payments.Command.UserCommand;
+using Hyip_Payments.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Hyip_Payments.Api.Controllers.User
+{
+    [ApiController]
+    [Route("api/user/register")]
+    public class RegisterController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RegisterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // POST: api/user/register
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> Register([FromBody] UserApplicationModel userApp)
+        {
+            // Map UserApplicationModel to RegisterUserCommand
+            var command = new RegisterUserCommand
+            {
+                Email = userApp.Email,
+                Password = userApp.Password,
+                Username = userApp.Email, // Or use another property if you have Username
+                EmailConfirmed = userApp.EmailConfirmed
+            };
+
+            var user = await _mediator.Send(command);
+            if (user == null)
+                return BadRequest("Registration failed.");
+            return Ok(user);
+        }
+    }
+}

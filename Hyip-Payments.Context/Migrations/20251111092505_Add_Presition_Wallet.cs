427bbfb5cdd2a269ef22a010f03f0ed3a3a6b61d
mariobot <mariobot113@hotmail.com>
2025-11-11 04:34:54 -0500

    Add migration to include Tenant.

---
+++ Hyip-Payments.Context/Migrations/20251111092505_Add_Presition_Wallet.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hyip_Payments.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Presition_Wallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserTenant",
+                columns: new[] { "Id", "Name", "IsActive", "CreatedAt" },
+                values: new object[] { 1, "HomeCompany", true, DateTime.UtcNow }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserTenant",
+                keyColumn: "Id",
+                keyValue: 1
+            );
+        }
+    }
+}

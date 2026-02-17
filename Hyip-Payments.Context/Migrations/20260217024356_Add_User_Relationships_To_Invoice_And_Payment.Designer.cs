c5e19f7495337835502222acc6ac5a15054b8835
mariobot <mariobot113@hotmail.com>
2026-02-16 21:55:20 -0500

    Add the migration to set user to do the action.

--- Hyip-Payments.Context/Migrations/20260217024356_Add_User_Relationships_To_Invoice_And_Payment.Designer.cs
+++ Hyip-Payments.Context/Migrations/20260217024356_Add_User_Relationships_To_Invoice_And_Payment.Designer.cs
@@ -4,6 +4,7 @@ using Hyip_Payments.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hyip_Payments.Context.Migrations
 {
     [DbContext(typeof(PaymentsDbContext))]
-    partial class PaymentsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260217024356_Add_User_Relationships_To_Invoice_And_Payment")]
+    partial class Add_User_Relationships_To_Invoice_And_Payment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Hyip-Payments.Context/Migrations/20260217024356_Add_User_Relationships_To_Invoice_And_Payment.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hyip_Payments.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_User_Relationships_To_Invoice_And_Payment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProcessedByUserId",
+                table: "PaymentTransaction",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByUserId",
+                table: "Invoice",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StatusInvoice",
+                table: "Invoice",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessedByUserId",
+                table: "PaymentTransaction");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "StatusInvoice",
+                table: "Invoice");
+        }
+    }
+}
